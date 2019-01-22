9cb5c814018fb91ddb35c78b1837563b6cbc3c9b
camclarke64 <camsyta81600@gmail.com>
2019-01-22 16:00:44 -0600

    Added a basic outline of code for Sam to approve or disapprove

--- ManipulatorCodeOutline/src/main/java/frc/robot/ManipCodeOutline.java
+++ ManipulatorCodeOutline/src/main/java/frc/robot/ManipCodeOutline.java
@@ -1,5 +1,35 @@
 package frc.robot;
 
 public class ManipCodeOutline {
-    
+
+    /**
+     * @param theta angle in radians -- this is what Java likes to use
+     * @return angle in degrees -- this is what we like to use
+     */
+    public double r2d(double theta) {
+        return theta * (180 / Math.PI);
+    }
+
+    /**
+     * @param degrees the set degree measurement we want to turn to
+     */
+    public void turnToAngle(double degrees) {
+        
+    }
+
+    /**
+     * We add code here for drive forward with vision
+     * Add a character to SmartDashboard whether or not we're following the straight line
+     */
+    public void centerDriveVision() {
+
+    }
+
+    /**
+     * We add code here (maybe Cam's?) for pnuematics push out
+     * Add a boolean return statement for whether or not the hatch kicked out
+     */
+    public void extendHatch() {
+
+    }
 }
