ac23ef752a049f0b37d7759c80005fa803134528
camclarke64 <camsyta81600@gmail.com>
2019-01-23 16:58:10 -0600

    Added some motor code for  reading encoder values

--- ManipulatorCodeOutline/src/main/java/frc/robot/Motor.java
+++ ManipulatorCodeOutline/src/main/java/frc/robot/Motor.java
@@ -1,10 +1,29 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Motor {
 
-    public void limitSwitchCode() {
-        
+    XboxController xBox;
+    CANSparkMax motor;
+    CANEncoder encoder;
+
+    public static final int deviceID = 1; //subject to change
+
+    public void robotInit() {
+        motor = new CANSparkMax(deviceID, MotorType.kBrushless);
+        encoder = motor.getEncoder();
+        xBox = new XboxController(Consts.xBoxPort);
+    }
+
+    public void teleopPeriodic() {
+        motor.set(xBox.getY());
+        SmartDashboard.putNumber("Encoder Position", encoder.getPosition());
+        SmartDashboard.putNumber("Encoder Velocity", encoder.getVelocity());
     }
 }
